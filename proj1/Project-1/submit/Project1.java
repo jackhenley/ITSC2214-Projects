67ba1560f99432a4846d280f66300b5e1d8a8f58
Jack H <braden355@icloud.com>
2025-08-25 19:39:59 -0400

    Added folder for project 1

---
+++ proj1/Project-1/submit/Project1.java
@@ -0,0 +1,23 @@
+import itsc2214.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Random;
+
+/**
+ * Implements Conway's Game of Life using a 2D array.
+ */
+public class Project1 implements GameOfLife {
+
+    // TODO Implement 2 constructors
+    public Project1() {
+        throw new UnsupportedOperationException("Unimplemented default constructor");        
+    }
+    public Project1(int row, int col) {
+        throw new UnsupportedOperationException("Unimplemented constructor with args");        
+    }
+
+    // TODO Implement all the methods fron the GameOfLife
+
+
+}
