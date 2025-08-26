6f4b1350ae7463a558ecfeaf6ce192df2917662a
Jack H <braden355@icloud.com>
2025-08-26 12:52:46 -0400

    Worked on stubbing out Project1

--- proj1/Project-1/submit/Project1.java
+++ proj1/Project-1/submit/Project1.java
@@ -4,6 +4,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.Random;
 
+//TODO: stub out
 /**
  * Implements Conway's Game of Life using a 2D array.
  */
@@ -19,5 +20,71 @@ public class Project1 implements GameOfLife {
 
     // TODO Implement all the methods fron the GameOfLife
 
+    /**
+     * randomInitialize - method that randomly initializes the grid 
+     * with live cells based on given probability. Number is generated
+     *  for each cell using Random class, and calling nextDouble() method. 
+     * If value returned is < alive probability, 
+     * cell is set to false (dead). Otherwise, cell is set to alive.
+     * 
+     * @param aliveProbability - double between (0.0-1.0) representing 
+     * probability of cell being initially alive.
+     */
+
+     public void randomInitialize(double aliveProbability) {
+
+     }
+
+
+     /**
+      * loadFromString - method that loads the initial grid
+      * config. from a string. String should contain the number  
+      * of rows and cols on the first line, followed by lines of 
+      * "." and 'O' characters representing the grid. 
+      *
+      * (Use scanner) 
+      *
+      * @param data - string containing grid data to be processed
+      */
+    public void loadFromString(String data) {
+
+    }
+
+    /**
+     * Load from file- Loads the initial grid config from a file. 
+     * Reads all data from file -> converts to string
+     * Once converted, this method calls the loadFromString 
+     * method to process the string.
+     * 
+     * @param fileName - name of file to be processed 
+     */
+    public void loadFromFile(String filename) throws FileNotFoundException {
+
+    }
+
+
+    /**
+     * countLiveNeighbors - method used to count the number of live 
+     * neighbors surrounding the cell at (r,c)
+     * 
+     * @param r - row index of the cell
+     * @param c - column index of the cell
+     * @return the number of neighbors nearby that are alive.
+     */
+    public int countLiveNeighbors(int r, int c) {
+        int result = 0;
+        return result;
+    }
+
+    /**
+     * nextGeneration - method used to determine the next generation
+     * of the grid based on Conway's rules. Traverses the internal
+     * 2-dimension boolean arayi, counting the number of neighbors for each cell
+     * by calling countLiveNeighbors, and sets the next generation grid
+     * position accordingly. 
+     */
+    public void nextGeneration() {
+
+    }
 
 }
