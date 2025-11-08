4178c06a87eb9503055ebe4342b4ec9b44fa679b
Jack H <braden355@icloud.com>
2025-11-08 18:46:41 -0500

    lots of changes today

--- proj4/submit/PixelatedImage.java
+++ proj4/submit/PixelatedImage.java
@@ -4,11 +4,12 @@ import java.util.Random;
 import java.util.Scanner;
 
 /**
- * Pixelated Image class - TODO: finish documentation
+ * Pixelated Image class.
+ * Class representing a grid of "colors" with built-in methods to
+ * manipulate/interact with those colors.
+ * 
  * @author Jack Henley
  */
-
-// TODO add documentation
 public class PixelatedImage {
 
    private int size;
@@ -18,48 +19,54 @@ public class PixelatedImage {
    /**
     * Constructor that creates a size x size integer array
     * and initializes it to 0 values.
+    * 
     * @param s - size of the array
     */
    public PixelatedImage(int s) {
+      size = s;
       grid = new int[size][size];
 
    }
 
    /**
-    * Constructor, tries to load the contents of filename by 
-    * calling loadFromFile. If loadFromFile throws an exception, this method (the constructor), 
-    * should catch it, and create a 5x5 integer array and initialize it to 0 values.
+    * Constructor, tries to load the contents of filename by calling
+    * loadFromFile().
+    * If loadFromFile throws an exception, this method (the constructor),
+    * should catch it, and create a 5x5 integer array and initialize it to 0
+    * values.
     */
-   public PixelatedImage(String filename)
-   {
+   public PixelatedImage(String filename) {
       try {
          loadFromFile(filename);
-      }catch(FileNotFoundException fne) {
+      } catch (FileNotFoundException fne) {
          grid = new int[5][5];
       }
    }
 
    /**
-    * Fills all positions in the grid with random values between 1 and 16.
-    */ 
+    * Method that fills all positions in the grid with random values between 1 and 16.
+    */
    public void loadRandomly() {
-
       Random rand = new Random();
-      for(int i = 0; i < grid.length; i ++) {
-         for(int j = 0; j < grid[0].length; j++) { //should work assuming all rows are same length (they are)
-            int value = rand.nextInt(1,16);
+      for (int i = 0; i < grid.length; i++) {
+         for (int j = 0; j < grid[0].length; j++) { // should work assuming all rows are same length (they are)
+            int value = rand.nextInt(16);
+            if(value == 0) {
+               value+=1;
+            }
             grid[i][j] = value;
          }
       }
    }
 
    /**
-    * Load the content of a csv file (comma separated values) into internal grid represntation.
+    * Load the content of a csv file (comma separated values) into internal grid
+    * represntation.
+    * 
     * @param inFile - csv file to load from
     * @throws FileNotFoundException - exception thrown if the given file does not exist.
     */
-   public void loadFromFile(String inFile) throws FileNotFoundException
-   {
+   public void loadFromFile(String inFile) throws FileNotFoundException {
       File myObj = new File(inFile);
       Scanner scan = new Scanner(myObj);
 
@@ -69,26 +76,32 @@ public class PixelatedImage {
       for (int i = 0; scan.hasNext() && i < getHeight(); i++) {
          line = scan.nextLine();
          String[] tokens = line.split(",");
-         for (int j = 0; j < Math.min(tokens.length,size); j++) {
+         for (int j = 0; j < Math.min(tokens.length, size); j++) {
             grid[i][j] = Integer.parseInt(tokens[j]);
          }
       }
       scan.close();
    }
 
-   // TODO add documentation
+   /**
+    * Method that returns the width of this grid.
+    * @return width of this grid.
+    */
    public int getWidth() {
       return grid.length;
    }
 
-   // TODO add documentation
-   public int getHeight()
-   {
+   /** 
+    * Method that returns the height of this grid.
+    * @return height of this grid.
+    */
+   public int getHeight() {
       return grid[0].length;
    }
 
    /**
     * Returns the color value stored at location - (row,col).
+    * 
     * @param row
     * @param col
     * @return
@@ -98,19 +111,20 @@ public class PixelatedImage {
    }
 
    /**
-    * Stores the color value at row and col. Color is expected to be between
-    * 0 and 15, if it isn't, do nothing (fail silently).
-    * @param row - row to fill
-    * @param col - col to fill
+    * Sets color value at row and col. Color is expected to be between 0-15.
+    * If it isn't, do nothing (fail silently).
+    * 
+    * @param row   - row to fill
+    * @param col   - col to fill
     * @param color - color (expect 0 <= color <= 15)
-    */ 
+    */
    public void setPixel(int row, int col, int color) {
-      if(color > 15 || color < 0) {
+      if (color > 15 || color < 0) {
          return;
-      }else {
+      } else {
          grid[row][col] = color;
       }
-      
+
    }
 
    /**
@@ -119,39 +133,56 @@ public class PixelatedImage {
     * version as origColor. Creates the visited array to
     * tag cells as they are visited.
     * 
-    * @param row row of the grid to visit
-    * @param col col of the grid to visit
+    * @param row       row of the grid to visit
+    * @param col       col of the grid to visit
     * @param fillColor color to use for replacing
     */
-   public void floodFill(int row, int col, int fillColor)
-   {
+   public void floodFill(int row, int col, int fillColor) {
       int origColor = grid[row][col];
       visited = new boolean[getHeight()][getWidth()];
       floodFill(row, col, origColor, fillColor);
    }
 
    /**
-    * Recusive routine that starts at row,col and if the color sotred there 
+    * Recusive routine that starts at row,col and if the color stored there
     * is origColor, replaces it and recursively calls it with the four adjacent
-    * neighbors (left, right, top bottom)
+    * neighbors (left, right, top bottom).
+    * 
     * @param row
     * @param col
     * @param origColor
     * @param fillColor
     */
    protected void floodFill(int row, int col, int origColor, int fillColor) {
-      //base case
-      if(grid[row][col] != origColor) {
-        return; 
-      }else {
-         //fill this element
-         grid[row][col] = fillColor;
+      // base cases
+      if (row >= this.size || col >= this.size) {
+         return;
+      }
+      if (grid[row][col] != origColor || visited[row][col]) {
+         return;
+      } else {
 
-         //recursive calls on left,right,top,bottom
-         floodFill(row+1, col, origColor, fillColor);
-         floodFill(row-1, col, origColor, fillColor);
-         floodFill(row, col+1, origColor, fillColor);
-         floodFill(row+1, col-1, origColor, fillColor);
+         // fill this element
+         grid[row][col] = fillColor;
+         visited[row][col] = true;
+
+         // recursive calls on left,right,top,bottom
+
+         if (col != this.getHeight() - 1) {
+            floodFill(row, col + 1, origColor, fillColor);
+         }
+
+         if (row != this.getWidth() - 1) {
+            floodFill(row + 1, col, origColor, fillColor);
+         }
+
+         if (row > 0) {
+            floodFill(row - 1, col, origColor, fillColor);
+         }
+
+         if (col > 0) {
+            floodFill(row + 1, col - 1, origColor, fillColor);
+         }
       }
    }
 }
--- proj4/submit/PixelatedImageTest.java
+++ proj4/submit/PixelatedImageTest.java
@@ -1,15 +1,17 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.io.FileWriter;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 /**
  * Unit tests for PixelatedImage - project 4
  * 
- * @author  YOUR NAME HERE
+ * @author Jack Henley
  */
-
 public class PixelatedImageTest {
-   private PixelatedImage runner = null;
+   private PixelatedImage runner;
+
    /**
     * setup() method, runs before each of your test methods.
     * Use this method to recreate the objects needed for
@@ -20,13 +22,117 @@ public class PixelatedImageTest {
       runner = new PixelatedImage(5);
    }
 
-   // TODO add documentation
+   /**
+    * testDefaults() method, tests the default constructor.
+    */
    @Test
    public void testDefaults() {
       assertEquals(5, runner.getWidth());
       assertEquals(5, runner.getHeight());
       for (int r = 0; r < runner.getHeight(); r++)
          for (int c = 0; c < runner.getWidth(); c++)
-            assertEquals(0, runner.getPixel(r,c));
+            assertEquals(0, runner.getPixel(r, c));
    }
+
+   /**
+    * testFromFile() method, tests constructor when loading from file.
+    */
+   @Test
+   public void testFromFile() {
+      String fileName = "testFile.txt";
+      String content = "20\n" + //
+            "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0\n" + //
+            "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0\n" + //
+            "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0\n" + //
+            "0,0,0,0,6,6,0,0,0,0,0,0,0,0,6,6,0,0,0,0\n" + //
+            "0,0,0,0,6,6,0,0,0,0,0,0,0,0,6,6,0,0,0,0\n" + //
+            "0,0,0,0,0,0,6,6,0,0,0,0,6,6,0,0,0,0,0,0\n" + //
+            "0,0,0,0,6,6,6,6,6,6,6,6,6,6,6,6,0,0,0,0\n" + //
+            "0,0,0,6,6,6,0,0,6,6,6,6,0,0,6,6,6,0,0,0\n" + //
+            "0,0,0,6,6,6,0,0,6,6,6,6,0,0,6,6,6,0,0,0\n" + //
+            "0,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,0\n" + //
+            "0,6,6,0,6,6,6,6,6,6,6,6,6,6,6,6,0,6,6,0\n" + //
+            "0,6,6,0,6,6,0,0,0,0,0,0,0,0,6,6,0,6,6,0\n" + //
+            "0,6,6,0,6,6,0,0,0,0,0,0,0,0,6,6,0,6,6,0\n" + //
+            "0,0,0,0,0,0,6,6,6,0,0,6,6,6,0,0,0,0,0,0\n" + //
+            "0,0,0,0,0,0,6,6,6,0,0,6,6,6,0,0,0,0,0,0\n" + //
+            "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0\n" + //
+            "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0\n" + //
+            "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0\n" + //
+            "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0\n" + //
+            "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0";
+      try {
+         FileWriter writer = new FileWriter(fileName);
+         writer.write(content);
+         writer.close();
+      } catch (IOException ioe) {
+      }
+      runner = new PixelatedImage(fileName);
+      assertEquals(20, runner.getHeight());
+      assertEquals(20, runner.getWidth());
+      // TODO: add more tests for files
+   }
+
+   /**
+    * testNoFile method, tests the loadFrom file method when file DNE.
+    */
+   @Test
+   public void testNoFile() {
+      runner = new PixelatedImage("fakeFile.txt");
+      assertEquals(5, runner.getHeight());
+      assertEquals(5, runner.getWidth());
+   }
+
+   /**
+    * testLoadRandomly(), test initializing grid using loadRandomly().
+    */
+   @Test
+   public void testLoadRandomly() {
+
+      runner.loadRandomly();
+
+      for (int i = 0; i < runner.getWidth(); i++) {
+         for (int j = 0; j < runner.getHeight(); j++) {
+            assertTrue(runner.getPixel(i, j) < 16 && runner.getPixel(i, j) > 0);
+         }
+      }
+   }
+
+   /**
+    * testSetPixel(), method to test setPixel() method.
+    */
+   @Test
+   public void testSetPixel() {
+      runner = new PixelatedImage(4);
+      runner.loadRandomly();
+
+      // set (2,2) to valid value
+      runner.setPixel(2, 2, 8);
+      assertEquals(8, runner.getPixel(2, 2));
+
+      // set (2,2) to negative value
+      runner.setPixel(2, 2, -3);
+      assertEquals(8, runner.getPixel(2, 2));
+
+      // set (2,2) to too high of a value
+      runner.setPixel(2, 2, 20);
+      assertEquals(8, runner.getPixel(2, 2));
+
+   }
+
+   /**
+    * testFloodFill, method that tests the floodFill method.
+    */
+   @Test
+   public void testFloodFill() {
+      runner.floodFill(0, 0, 7);
+
+      for (int i = 0; i < runner.getWidth(); i++) {
+         for (int j = 0; j < runner.getHeight(); j++) {
+            assertEquals(runner.getPixel(i, j), 7);
+         }
+      }
+
+   }
+
 }
