8ebd096eaa934f7527ed01b969311c7068980058
Jack H <braden355@icloud.com>
2025-11-28 15:19:04 -0500

    Got started on most of the methods

--- proj-5/submit/AutoComplete.java
+++ proj-5/submit/AutoComplete.java
@@ -10,37 +10,59 @@ import itsc2214.*;
  */
 public class AutoComplete extends AVLTree<String> {
 
+    private int size = 0;
+
     /**
-     * Constructor
+     * This is the constructor for the class. It takes the name of a file containing
+     * words and reads them in an internal intance variable of type BinarySearchTree<String>
      */
     public AutoComplete(String filename)
     {
+        super();
         // call the superclass constructor
         // load the contents of the file
         // catch a FileNotFound exception and don't fail
         // keep track of number of words loaded
+        try {
+            File file = new File("filename");
+            Scanner scanIn = new Scanner(file);
+            while(scanIn.hasNextLine()) {
+                String word = scanIn.nextLine();
+                this.add(word);
+                this.size += 1;
+            }
+            scanIn.close();
+        }catch(FileNotFoundException fne) {
+            //what should we do here
+            System.exit(0);
+        }
     }
 
     /**
-     * numWords
+     * Method that returns the number of words in the file.
+     * @return number of words in the file.
      */
     public int numWords()
     {
-        // TODO return number of words
-        return 0;
+        //TODO: return the number of words
+        return this.size;      
     }
 
+
     /**
-     * validSpelling
+     * Checks the internal bst for the given word.
+     * @return true if word exists in the tree, false otherwise.
      */
     public boolean validSpelling(String w)
     {
         // TODO check if w exists in the tree
-        return false;
+        return this.contains(w);
     }
 
     /**
-     * possibleNextLetter
+     * Given a list of words, all with a common prefix, find the set of next letters from word passed as args.
+     * @return hashmap containing int, char pair, where the char is the next letter
+     * and the int is the number of occurences.
      */
     public HashMap<Character, Integer> possibleNextLetter(
             ArrayList<String> list, String prefix)
@@ -48,16 +70,32 @@ public class AutoComplete extends AVLTree<String> {
         // TODO build a hashmap of the "next" letter and a count
         // count all the words that have the next letter
         // return a hashmap
-        return null;
+        HashMap<Character, Integer> prefixes = new HashMap<>();
+        for(int i = 0; i < list.size(); i++) {
+            String word = list.get(i);
+            if(word.startsWith(prefix)) {
+                String newWord = word.substring(prefix.length());
+                Character nextChar = newWord.charAt(0);
+                if(prefixes.containsKey(nextChar)) {
+                    prefixes.put(nextChar, prefixes.get(nextChar) + 1);
+                }else {
+                    prefixes.put(nextChar, 1);
+                }
+            }
+        }
+        return prefixes;
     }
     /**
-     * autocomplete 
+     * Does a tree traversal of the internal tree looking for
+     * all the words in the tree that start with the prefix. 
+     * @return an ArrayList<String> with the words found.
      */
     public ArrayList<String> autocomplete(String pre, int limit)
     {
         // TODO call the recursive version below
         // sort the results in increasing order of word length
         // return only the first 'limit' results
+        
         return null;
     }
 
