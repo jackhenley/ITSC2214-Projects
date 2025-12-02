bc1c856ba7ffca12309b5dcd999b10341cd93b18
Jack Henley <braden355@icloud.com>
2025-12-02 02:25:03 -0500

    Got AutoComplete.java to good point & tester is coming along

--- proj5/submit/AutoComplete.java
+++ proj5/submit/AutoComplete.java
@@ -24,7 +24,7 @@ public class AutoComplete extends AVLTree<String> {
         // catch a FileNotFound exception and don't fail
         // keep track of number of words loaded
         try {
-            File file = new File("filename");
+            File file = new File(filename);
             Scanner scanIn = new Scanner(file);
             while(scanIn.hasNextLine()) {
                 String word = scanIn.nextLine();
@@ -34,7 +34,8 @@ public class AutoComplete extends AVLTree<String> {
             scanIn.close();
         }catch(FileNotFoundException fne) {
             //what should we do here
-            System.exit(0);
+            System.err.println("Invalid file name "+fne);
+            
         }
     }
 
@@ -61,6 +62,9 @@ public class AutoComplete extends AVLTree<String> {
 
     /**
      * Given a list of words, all with a common prefix, find the set of next letters from word passed as args.
+     * 
+     * @param list - list of words
+     * @param prefix - prefix for words to have in common
      * @return hashmap containing int, char pair, where the char is the next letter
      * and the int is the number of occurences.
      */
@@ -85,28 +89,76 @@ public class AutoComplete extends AVLTree<String> {
         }
         return prefixes;
     }
+
+
     /**
-     * Does a tree traversal of the internal tree looking for
-     * all the words in the tree that start with the prefix. 
-     * @return an ArrayList<String> with the words found.
+     * Does a tree traversal to find all words in the tree that start with pre.
+     * This is achieved by calling the private, recursive autocomplete method. 
+     * Once the recursive method returns an array list containing all words that start
+     * with pre, this method sorts those words based on their length.
+     *  
+     * @param pre - prefix to check for 
+     * @param limit - number of words to limit arrayList to  
+     * @return the ArrayList<String> with the words found.
      */
     public ArrayList<String> autocomplete(String pre, int limit)
     {
         // TODO call the recursive version below
         // sort the results in increasing order of word length
         // return only the first 'limit' results
+
+
+        AVLNode<String> root = this.getRoot();
+
+        ArrayList<String> list = new ArrayList<>();
+        autocomplete(root, pre, list);
+
+        //list should be populated & unsorted now
+
         
-        return null;
+        //TODO: might need to add comparator as 2nd arg
+        Collections.sort(list);
+
+        //only want to return 'limit' amount of entries        
+        ArrayList<String> finalList = new ArrayList<>(); 
+        for(int i = 0; i < limit; i++) {
+            finalList.add(list.get(i));
+        }
+
+        return finalList;
     }
 
     /**
-     * autocomplete 
+     * Private, recursive autocomplete method that populates the unsorted array list.
+     * 
+     * @param node - root node of AVLTree 
+     * @param pre - prefix to compare against
      */
     private void autocomplete(AVLNode<String> node, String pre,
         ArrayList<String> lst)
     {
+
         // TODO recursive traversal of the tree looking
         // for words that start with pre, accumulating them
         // in lst
+
+        if(node == null) {
+            return;
+        }
+
+        String word = node.getValue();
+
+        if(word.compareTo(pre) >= 0) {
+            autocomplete(node.getLeft(), pre, lst);
+        }
+
+        if(word.startsWith(pre)) {
+            lst.add(word);
+        }
+
+        if(word.startsWith(pre) || word.compareTo(pre) < 0) {
+            autocomplete(node.getRight(), pre, lst);
+        }
+
     }
 }
--- proj5/submit/AutoCompleteTest.java
+++ proj5/submit/AutoCompleteTest.java
@@ -2,8 +2,11 @@ import org.junit.*;
 import static org.junit.Assert.*;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+import java.io.*;
 /**
  * Unit test for the auto-complete project.
+ * @author Jack Henley
  */
 public class AutoCompleteTest
 {
@@ -17,14 +20,121 @@ public class AutoCompleteTest
     @Before
     public void setup() {
         runner = new AutoComplete("words.txt");
+        ArrayList<String> nextList = runner.autocomplete("cho", 11);
+        HashMap<Character, Integer> map = runner.possibleNextLetter(nextList, "cho");
     }
 
     /**
-     * Basic test
-     */
+     * Basic test format
+    
     @Test
     public void testSomething()
     {
         fail("You have to provide test cases");
     }
+    */
+
+
+    /**
+     * Tests the constructor with an invalid filename. 
+     * 
+     */
+    @Test
+    public void testInvalidFileName() {
+        AutoComplete fakeRunner = new AutoComplete("invalid.txt");
+        assertEquals(fakeRunner.numWords(),0);
+    }
+
+    /**
+     * TODO: probably don't need to test this case
+     * Tests the constructor with a null filename
+     * 
+     *
+    @Test
+    public void testNullFileName () {
+        try {
+            AutoComplete nullRunner = new AutoComplete(null);
+            fail("Should throw null pointer exception");
+        }catch (NullPointerException npe) {
+            
+        }
+    }
+    */
+
+
+    /**
+     * Tests that the num words in the AVLTree is equal to the num words in the file.
+     */
+    @Test
+    public void testNumWords() {
+        assertEquals(5544, runner.numWords());
+    }
+
+
+    /**
+     * Tests that the valid spelling method works with a properly spelled word.
+     */
+    @Test 
+    public void testValidSpellingProper () {
+        assertTrue(runner.validSpelling("chocolate"));
+    }
+
+    /**
+     * Tests that the valid spelling method works with an incorrectly spelled word.
+     */
+    @Test
+    public void testValidSpellingIncorrect () {
+        //TODO: assertFalse?
+        assertTrue(!runner.validSpelling("choocolate"));
+
+    }
+
+    /**
+     * Tests possible next letters and checks their frequencies.
+     */
+    @Test 
+    public void testPossibleNextLetterValid () {
+        assertTrue(map.containsKey("r"));
+    }
+
+
+    /**
+     * Test that verifies a word that shouldn't be in the hashmap isn't in the hashmap.
+     * 
+     */
+    @Test
+    public void testPossibleNextLetterInvalid () {
+
+    }
+
+
+    /**
+     * Tests the autocomplete method when the prefix is valid. 
+     * 
+     */
+    @Test 
+    public void testAutocompleteValidPre () {
+
+    }
+
+
+    /**
+     * Tests the autocomplete method when the prefix is invalid. 
+     * 
+     */
+    @Test
+    public void testAutocompleteInvalidPre () {
+
+    }
+
+
+    /**
+     * Test that ensures arraylist returned from autocomplete is sorted.
+     * 
+     */
+    @Test
+    public void ensureSorted () {
+
+    }
+
 }
